c15b62f4b4a0eef6184ebe5d370628adc5ceaf6e
AtanasovMiro <miroslav.hristov.atanasov@gmail.com>
2025-02-22 00:03:53 +0200

    added a new sum class

--- example.java
+++ example.java
@@ -1,4 +1,4 @@
-public class welcome {
+public class example {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a test");
---
+++ newClass.java
@@ -0,0 +1,11 @@
+
+public class newClass {
+	public static void main(String[] args) {
+		int result = sumInt(5, 20); // Call the method and store the result
+        System.out.println("Sum: " + result); // Print the result
+    }
+
+    static int sumInt(int a, int b) {
+        return a + b;
+    }
+}
