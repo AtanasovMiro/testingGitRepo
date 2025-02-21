2a2fb9c10c2a504b153246d1201e5827b8087256
AtanasovMiro <miroslav.hristov.atanasov@gmail.com>
2025-02-21 23:55:17 +0200

    Updated .java file

--- welcome.java
+++ welcome.java
@@ -1,5 +1,7 @@
-public class Main {
+public class welcome {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is a test");
+        System.out.println("Adding some more text");
     }
 }
