69a7713d0ac9c7df72de307fd74d17d9276fa1d4
Miroslav Atanasov <125657978+AtanasovMiro@users.noreply.github.com>
2025-02-21 23:58:46 +0200

    Update and rename welcome.java to example.java

--- example.java
+++ example.java
@@ -3,5 +3,9 @@ public class welcome {
         System.out.println("Hello, World!");
         System.out.println("This is a test");
         System.out.println("Adding some more text");
+
+        for(int a=0; a<10; a++){
+            System.out.println(a);
+        }
     }
 }
